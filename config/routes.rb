04a8836a07d31b16a08a1bc0f50bb262b9d7b00c
sridhar16899 <sridhar.arumugam@tringapps.com>
2021-04-07 15:26:17 +0530

    jwt add

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
       resources :tables
     end
   end
+  post 'authenticate', to: 'authentication#authenticate'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_18_091955) do
+ActiveRecord::Schema.define(version: 2021_04_07_074253) do
 
   create_table "tables", force: :cascade do |t|
     t.string "name"
     t.string "email"
     t.string "phone"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
