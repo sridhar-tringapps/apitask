896296b274c193117625895b0d966767db78af40
sridhar16899 <sridhar.arumugam@tringapps.com>
2021-04-12 19:12:51 +0530

    frontend

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,6 @@ Rails.application.routes.draw do
     end
   end
   post 'authenticate', to: 'authentication#authenticate'
+  get 'authenticate', to: 'authentication#authenticate'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
---
+++ frontend/assets/js/index.js
@@ -0,0 +1,47 @@
+$("#submit").on("click",function(){
+    $div=$("#div");
+    var user = 
+    {
+      email: $div.find('input.email').val(),
+      password: $div.find('input.password').val(),
+    }
+    console.log(user)
+  
+    function jwt(token)
+    {
+      jwttoken = token;
+      console.log("token",jwttoken)
+      sessionStorage.setItem("uniquetoken",jwttoken)
+      tkn=sessionStorage.getItem('uniquetoken'),
+      console.log("tkn",tkn)
+      $.ajax({
+        url: "http://localhost:3000/api/v1/tables",
+        headers: {
+          "Content-Type": "application/json",
+          "Accept": "application/json",
+          "Authorisation": "Bearer "+tkn
+        },
+        type:"GET",
+        datatype: 'JSON',
+        success: function (temp){
+          console.log(temp.data)
+          window.location.href="sample.html?t="+tkn;
+        },
+        error: function(temp)
+        {
+          console.log(temp.token)
+        }
+      });
+    }
+    $.ajax({
+      type:"POST",
+      datatype: 'JSON',
+      url: "http://localhost:3000/authenticate",
+      data:user,
+      success: function(temp){
+        console.log(temp.auth_token);
+        token = temp.auth_token;
+        jwt(token);
+      }
+    });
+  });
